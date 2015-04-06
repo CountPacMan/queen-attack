e7ffd41202ccf4c2e2b69a249b28437eae6b5d44
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-06 10:34:35 -0700

    add front end

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,37 @@ function queenAttack(queenPos, otherPos) {
   return (queenPos[0] === otherPos[0]) || (queenPos[1] === otherPos[1]) || (Math.abs(queenPos[0] - otherPos[0]) === Math.abs(queenPos[1] - otherPos[1]));
 }
 
+function isValid(element) {
+  return element > 0 && element < 9;
+}
+
 jQuery(document).ready(function() {
+
+  $("#queen-attack").submit(function(event) {
+    $("#error").empty();
+    var queen_row = parseInt($("#queen_row").val());
+    var queen_col = parseInt($("#queen_col").val());
+    var attack_row = parseInt($("#attack_row").val());
+    var attack_col = parseInt($("#attack_col").val());
+
+    var queenPos = [queen_row, queen_col];
+    var otherPos = [attack_row, attack_col];
+
+    if (!queenPos.every(isValid)) {
+      $("#error").prepend("<p>Queen position must be within 1 and 8</p>");
+    }
+    if (!otherPos.every(isValid)) {
+      $("#error").prepend("<p>Attack position must be within 1 and 8</p>");
+    }
+
+    if (queenPos.every(isValid) && otherPos.every(isValid)) {
+
+    } else {
+
+    }
+
+    event.preventDefault();
+  });
+
 
 });
