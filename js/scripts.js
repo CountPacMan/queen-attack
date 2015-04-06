edfef8cf4585c3af5c2d50c0288bcde775550cec
Daniel Toader & Tom Mertz <student@epicodus.com>
2015-04-06 09:55:26 -0700

    add logic for one diagonal direction

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function queenAttack(queenPos, otherPos) {
   // row is 0 element of array, column is 1
-  return (queenPos[0] === otherPos[0]) || (queenPos[1] === otherPos[1]);
+  return (queenPos[0] === otherPos[0]) || (queenPos[1] === otherPos[1]) || (queenPos[0] - otherPos[0] === queenPos[1] - otherPos[1]);
 }
 
 jQuery(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -4,10 +4,18 @@ describe('queenAttack', function() {
   });
 
   it("is true if the queen is in the same row as the other piece", function() {
-    expect(queenAttack([1,1], [1, 2])).to.equal(true);
+    expect(queenAttack([1, 1], [1, 2])).to.equal(true);
   });
 
   it("is true if the queen is in the same column as the other piece", function() {
-    expect(queenAttack([6,2], [1, 2])).to.equal(true);
+    expect(queenAttack([6, 2], [1, 2])).to.equal(true);
+  });
+
+  it("is true if the other piece is diagonally down-right from the queen", function() {
+    expect(queenAttack([3, 5], [5, 7])).to.equal(true);
+  });
+
+  it("is true if the other piece is diagonally up-left from the queen", function() {
+    expect(queenAttack([3, 5], [2, 4])).to.equal(true);
   });
 });
